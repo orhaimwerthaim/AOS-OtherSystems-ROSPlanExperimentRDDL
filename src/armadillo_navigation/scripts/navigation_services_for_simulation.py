6513cec42c45fb5b61d0af1c3f2689fb1359e0ee
TalFeiner <talfe@post.bgu.ac.il>
2020-07-09 13:52:38 +0300

    The best work was done today!

--- src/armadillo_navigation/scripts/navigation_services_for_simulation.py
+++ src/armadillo_navigation/scripts/navigation_services_for_simulation.py
@@ -42,7 +42,112 @@ def _callback_navigate_elevator(req):
         rospy.logerr("The robot failed to reach the elevator")
         # ser_messageResponse(False)
 
+def _callback_navigate_auditorium(req):
+
+    # define a client to send goal requests to the move_base server through a SimpleActionClient
+    ac = actionlib.SimpleActionClient("move_base", MoveBaseAction)
+    # wait for the action server to come up
+    while(not ac.wait_for_server(rospy.Duration.from_sec(5.0))):
+        rospy.logwarn("Waiting for the move_base action server to come up")
+    '''while(not ac_gaz.wait_for_server(rospy.Duration.from_sec(5.0))):
+        rospy.loginfo("Waiting for the move_base_simple action server to come up")'''
+    goal = MoveBaseGoal()
+    #set up the frame parameters
+    goal.target_pose.header.frame_id = "/map"
+    goal.target_pose.header.stamp = rospy.Time.now()
+    # moving towards the goal*/
+    goal.target_pose.pose.position =  Point(12.557, -3.291, 0)
+    orientation = tf.transformations.quaternion_from_euler(0, 0, 1.685)
+    goal.target_pose.pose.orientation.x = orientation[0]
+    goal.target_pose.pose.orientation.y = orientation[1]
+    goal.target_pose.pose.orientation.z = orientation[2]
+    goal.target_pose.pose.orientation.w = orientation[3]
+
+    rospy.loginfo("Sending goal location ...")
+    ac.send_goal(goal)	
+    ac.wait_for_result(rospy.Duration(60))
+
+    if(ac.get_state() ==  GoalStatus.SUCCEEDED):
+        rospy.loginfo("You have reached the elevator")
+        # ser_messageResponse(True)
+    else:
+        rospy.logerr("The robot failed to reach the elevator")
+        # ser_messageResponse(False)
+
+def _callback_navigate_lab_211(req):
+
+    # define a client to send goal requests to the move_base server through a SimpleActionClient
+    ac = actionlib.SimpleActionClient("move_base", MoveBaseAction)
+    # wait for the action server to come up
+    while(not ac.wait_for_server(rospy.Duration.from_sec(5.0))):
+        rospy.logwarn("Waiting for the move_base action server to come up")
+    '''while(not ac_gaz.wait_for_server(rospy.Duration.from_sec(5.0))):
+        rospy.loginfo("Waiting for the move_base_simple action server to come up")'''
+    goal = MoveBaseGoal()
+    #set up the frame parameters
+    goal.target_pose.header.frame_id = "/map"
+    goal.target_pose.header.stamp = rospy.Time.now()
+    # moving towards the goal*/
+    goal.target_pose.pose.position =  Point(0.320, 1.148, 0)
+    orientation = tf.transformations.quaternion_from_euler(0, 0, -1.621)
+    goal.target_pose.pose.orientation.x = orientation[0]
+    goal.target_pose.pose.orientation.y = orientation[1]
+    goal.target_pose.pose.orientation.z = orientation[2]
+    goal.target_pose.pose.orientation.w = orientation[3]
+
+    rospy.loginfo("Sending goal location ...")
+    ac.send_goal(goal)	
+    ac.wait_for_result(rospy.Duration(60))
+
+    if(ac.get_state() ==  GoalStatus.SUCCEEDED):
+        rospy.loginfo("You have reached the elevator")
+        # ser_messageResponse(True)
+    else:
+        rospy.logerr("The robot failed to reach the elevator")
+        # ser_messageResponse(False)
+
+def _callback_navigate_corridor(req):
+
+    # define a client to send goal requests to the move_base server through a SimpleActionClient
+    ac = actionlib.SimpleActionClient("move_base", MoveBaseAction)
+    # wait for the action server to come up
+    while(not ac.wait_for_server(rospy.Duration.from_sec(5.0))):
+        rospy.logwarn("Waiting for the move_base action server to come up")
+    '''while(not ac_gaz.wait_for_server(rospy.Duration.from_sec(5.0))):
+        rospy.loginfo("Waiting for the move_base_simple action server to come up")'''
+    goal = MoveBaseGoal()
+    #set up the frame parameters
+    goal.target_pose.header.frame_id = "/map"
+    goal.target_pose.header.stamp = rospy.Time.now()
+    # moving towards the goal*/
+    goal.target_pose.pose.position =  Point(4.100, -4.217, 0)
+    orientation = tf.transformations.quaternion_from_euler(0, 0, 1.300)
+    goal.target_pose.pose.orientation.x = orientation[0]
+    goal.target_pose.pose.orientation.y = orientation[1]
+    goal.target_pose.pose.orientation.z = orientation[2]
+    goal.target_pose.pose.orientation.w = orientation[3]
+
+    rospy.loginfo("Sending goal location ...")
+    ac.send_goal(goal)	
+    ac.wait_for_result(rospy.Duration(60))
+
+    if(ac.get_state() ==  GoalStatus.SUCCEEDED):
+        rospy.loginfo("You have reached the elevator")
+        # ser_messageResponse(True)
+    else:
+        rospy.logerr("The robot failed to reach the elevator")
+        # ser_messageResponse(False)
 
 rospy.Service("/elevator_go", ser_message, _callback_navigate_elevator)
 rospy.loginfo("navigation service is waiting for request...")
+
+rospy.Service("/auditorium_go", ser_message, _callback_navigate_auditorium)
+rospy.loginfo("navigation service is waiting for request...")
+
+rospy.Service("/lab_211_go", ser_message, _callback_navigate_lab_211)
+rospy.loginfo("navigation service is waiting for request...")
+
+rospy.Service("/corridor_go", ser_message, _callback_navigate_corridor)
+rospy.loginfo("navigation service is waiting for request...")
+
 rospy.spin()
